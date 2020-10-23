3ae2038dabb3581b494e27f85a293b1d0c3dcf6a
Raghavendra Galeppa <Raghavendrar404@gmail.com>
2020-10-23 21:33:23 +0530

    A Simple hashing function which can create 101 unique values for unique string

---
+++ table_lookup.c
@@ -0,0 +1,25 @@
+/*
+	A Simple table lookup function
+
+   */
+
+#include <stdio.h>
+#define HASHVAL 101
+
+unsigned create_hash(char *s) {
+	/* Create a simple hash for that string */
+
+	unsigned hash;
+	for (hash=0; *s != '\0'; s++) {
+		hash = *s + 31 * hash;
+	}
+	return hash % HASHVAL;
+}
+
+int main() {
+	unsigned hash_val;	
+	char *s = "Raghave";
+	hash_val = create_hash(s);
+	printf("%s: , Hash Val: %u\n",s,hash_val);
+	return 0;
+}
